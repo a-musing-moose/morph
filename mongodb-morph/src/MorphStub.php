e693d66b4477ee1fb7f199a700b772d76c2bc394
XirisR <XirisR@406649b4-5d4b-11de-8abc-d1c337b90d21>
2009-09-20 11:16:17 +0000

    hardcoding file paths for greater autoloader efficiency
    git-svn-id: http://mongodb-morph.googlecode.com/svn/trunk@31 406649b4-5d4b-11de-8abc-d1c337b90d21

--- mongodb-morph/src/MorphStub.php
+++ mongodb-morph/src/MorphStub.php
@@ -1,84 +1,62 @@
 <?php
 Phar::mapPhar();
 
-//handle running from command line
-if(isset($argc) && ($argc > 1)){
-    $command = strtolower($argv[1]);
-    switch($command){
-        case '-?':
-        case '-h':
-            showInfo();
-            break;
-        case '-l':
-            showInfo(true);
-            break;
-        case '-v':
-            showVersion();
-            break;
-    }
-    die();
-}
-
 /**
- * Simple autoloader for the morph package
+ * Autoloader for the morph package
  *
- * @param string $className
- * @return boolean
  */
-function morphAutoloader($className)
+class MorphAutoloader
 {
-    $included = false;
-    $path = 'phar://' . str_replace('_', '/', $className) . '.php';
-    if (file_exists($path)) {
-        require $path;
-        $included = true;
+
+    /**
+     * A static array of classes
+     *
+     * @var array
+     */
+    private static $classes = array(
+        'Morph_Utils'                   => 'phar://Morph/Utils.php',
+        'Morph_Iterator'                => 'phar://Morph/Iterator.php',
+        'Morph_Compare_NumericProperty' => 'phar://Morph/Compare/NumericProperty.php',
+        'Morph_Compare_Property'        => 'phar://Morph/Compare/Property.php',
+        'Morph_Query'                   => 'phar://Morph/Query.php',
+        'Morph_Object'                  => 'phar://Morph/Object.php',
+        'Morph_ICompare'                => 'phar://Morph/ICompare.php',
+        'Morph_Property_HasMany'        => 'phar://Morph/Property/HasMany.php',
+        'Morph_Property_Date'           => 'phar://Morph/Property/Date.php',
+        'Morph_Property_ComposeMany'    => 'phar://Morph/Property/ComposeMany.php',
+        'Morph_Property_HasOne'         => 'phar://Morph/Property/HasOne.php',
+        'Morph_Property_Enum'           => 'phar://Morph/Property/Enum.php',
+        'Morph_Property_Integer'        => 'phar://Morph/Property/Integer.php',
+        'Morph_Property_File'           => 'phar://Morph/Property/File.php',
+        'Morph_Property_Float'          => 'phar://Morph/Property/Float.php',
+        'Morph_Property_ComposeOne'     => 'phar://Morph/Property/ComposeOne.php',
+        'Morph_Property_String'         => 'phar://Morph/Property/String.php',
+        'Morph_Property_Generic'        => 'phar://Morph/Property/Generic.php',
+        'Morph_Collection'              => 'phar://Morph/Collection.php',
+        'Morph_Query_Property'          => 'phar://Morph/Query/Property.php',
+        'Morph_PropertySet'             => 'phar://Morph/PropertySet.php',
+        'Morph_Storage'                 => 'phar://Morph/Storage.php'
+    );
+
+    /**
+     * Loader function
+     *
+     * @param string $className
+     * @return boolean
+     */
+    public static function load($className)
+    {
+        $isLoaded = false;
+        if (isset(self::$classes[$className])) {
+            include self::$classes[$className];
+            $isLoaded = true;
+        }
+        return $isLoaded;
     }
-    return $included;
+
 }
 
 //register the autoloader
-spl_autoload_register('morphAutoloader');
-
-/**
- * Displays the build number
- *
- * @return void
- */
-function showVersion()
-{
-    $p = new Phar('phar://'.__FILE__);
-    $m = $p->getMetadata();
-    fwrite(STDOUT, "Morph Build: " . $m['Build'] . "\n");
-}
-
-/**
- * Displays the phar file metadata and optionally a file list
- *
- * @param boolean $includeFileList
- * @return void
- */
-function showInfo($includeFileList = false)
-{
-    $p = new Phar('phar://'.__FILE__);
-    fwrite(STDOUT, "Morph Library\n-------------\n");
-    foreach ($p->getMetadata() as $field => $value){
-        $field = str_pad($field, 16, ' ', STR_PAD_RIGHT);
-        fwrite(STDOUT, "$field: $value\n");
-    }
-
-    if ($includeFileList === true) {
-        fwrite(STDOUT, "\nFile List\n---------\n");
-        $i = new RecursiveIteratorIterator($p);
-        $baseName = 'phar://' . __FILE__ . '/';
-        foreach($i as $file){
-            $name = $file->getPath() .'/'. $file->getFilename();
-            $name = str_replace($baseName, '', $name);
-            fwrite(STDOUT, "$name\n");
-        }
-    }
-
-    fwrite(STDOUT, "\n");
-
-}
+spl_autoload_register(array('MorphAutoloader', 'load'));
 
 __HALT_COMPILER();
