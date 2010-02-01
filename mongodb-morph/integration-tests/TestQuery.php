1bcc90fee8ddc52a5a2b2336d5ac97a10b7b4f07
XirisR <XirisR@406649b4-5d4b-11de-8abc-d1c337b90d21>
2010-02-01 13:24:44 +0000

    adding tests for querying
    git-svn-id: http://mongodb-morph.googlecode.com/svn/trunk@116 406649b4-5d4b-11de-8abc-d1c337b90d21

---
+++ mongodb-morph/integration-tests/TestQuery.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author Jonathan Moss <xirisr@gmail.com>
+ * @copyright 2010 Jonathan Moss
+ */
+
+require_once dirname(__FILE__).'/../Morph.phar';
+require_once dirname(__FILE__).'/MongoUnit/TestCase.php';
+require_once dirname(__FILE__).'/test-objects/Child.php';
+
+class TestSingleObject extends MongoUnit_TestCase
+{
+
+    public function setup()
+    {
+        parent::setup();
+        Morph_Storage::init($this->getDatabase());
+        //load data from file to query against
+        $this->loadJsonFileDatasetIntoCollection('Child', dirname(__FILE__). '/fixtures/children.json');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        Morph_Storage::deInit();
+    }
+
+    public function testCanFindByValue()
+    {
+        $child = new Child();
+        $query = new Morph_Query();
+        $query->property('Name')->equals('a_musing_moose');
+        $results = $child->findByQuery($query);
+        $this->assertEquals(1, $results->totalCount());
+    }
+
+    public function testCanFindByLike()
+    {
+        $child = new Child();
+        $query = new Morph_Query();
+        $query->property('Name')->like('moose');
+        $results = $child->findByQuery($query);
+        $this->assertEquals(4, $results->totalCount());
+    }
+}
